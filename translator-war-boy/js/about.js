668c13bdb83def6f446b590036273b35a9bf1e10
Epu Baal <herdin86@gmail.com>
2021-04-08 15:04:16 +0900

    add buy me a coffe

--- translator-war-boy/js/about.js
+++ translator-war-boy/js/about.js
@@ -3,7 +3,7 @@
   logger.log('hello, about js');
 
   document.querySelector('#toggle-donation').addEventListener('click', () => {
-    let style = document.querySelector('#img-donation').style;
+    let style = document.querySelector('.donation-container').style;
     if(style.display) style.display = '';
     else style.display = 'none';
   });
